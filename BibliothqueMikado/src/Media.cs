f6e0716736c02c5290e973a987043f73fce79863
Whiteyeas <gabriel-le-jedi@hotmail.fr>
2020-02-14 17:49:23 +0100

    ajout classes

---
+++ BibliothqueMikado/src/Media.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliothqueMikado.src
+{
+    class Media
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BibliothqueMikado/src/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliothqueMikado.src
+{
+    class User
+    {
+        public string Name { get; set; }
+
+        private string Password { get; set; }
+
+        public string Rank { get; set; }
+
+        public List<int> Favori { get; set; }
+
+        public User(string name, string password, string rank)
+        {
+            Name = name;
+            Password = password;
+            Rank = rank;
+            Favori = new List<int>();
+        }
+
+        public User(string name, string rank, List<int> favori)
+        {
+            Name = name;
+            Rank = rank;
+            Favori = favori;
+        }
+    }
+}
